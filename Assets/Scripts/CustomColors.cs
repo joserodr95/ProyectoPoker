dfcaee15e2ebfc753514a1a32efa4cf598544d22
Jose Rodriguez <joserodriguezrivas95@gmail.com>
2022-03-06 19:06:37 +0100

    Added CustomColors class

---
+++ Assets/Scripts/CustomColors.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public static class CustomColors
+{
+    public static readonly Color halfTransparentWhite = new Color(1f, 1f, 1f, 0.5f);
+    public static readonly Color cardbackRed = new Color(0.773f, 0.179f, 0.179f, 1f);
+}
--- Assets/Scripts/Selectable.cs
+++ Assets/Scripts/Selectable.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using static CustomColors;
 
 public class Selectable : MonoBehaviour {
 
@@ -7,19 +8,17 @@ public class Selectable : MonoBehaviour {
     [field: SerializeField]
     public bool Selected { get; set; } = false;
 
-    private Color halfTransparent = new Color(1f, 1f, 1f, 0.5f);
-    private Color redBack = new Color(0.7735849f, 0.1788003f, 0.1788003f, 1f);
-
     // Update is called once per frame
     void Update() {
         if (Selected) {
-            this.GetComponent<SpriteRenderer>().color = halfTransparent;
+            this.GetComponent<SpriteRenderer>().color = CustomColors.halfTransparentWhite;
             this.GetComponent<Transform>().localPosition = new Vector3(this.GetComponent<Transform>().position.x, 1f, 0f);
         } else {
             if (FaceUp) {
                 this.GetComponent<SpriteRenderer>().color = Color.white;
-            } else {
-                this.GetComponent<SpriteRenderer>().color = redBack;
+            } else
+            {
+                this.GetComponent<SpriteRenderer>().color = CustomColors.cardbackRed;
             }
             this.GetComponent<Transform>().localPosition = new Vector3(this.GetComponent<Transform>().position.x, 0f, 0f);
         }
